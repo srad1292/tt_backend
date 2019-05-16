b72cfa3ad12e92db774ad42ca0c38faaf75cfa25
Sam Radford <smr1292@yahoo.com>
2019-05-16 15:21:20 -0400

    added route to get all open tickets

--- tt_api/routes/ticket.py
+++ tt_api/routes/ticket.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from flask_jsonpify import jsonify
 # from errors.errors import PageNotFound
 from tt_api.modules.ticket import ticket as ticket_module
@@ -12,3 +12,15 @@ def get(id=0):
     # raise PageNotFound('Page Not Found', status_code=404)
     ticket = ticket_module.getTicket(id)
     return jsonify({'ticket': ticket}) 
+
+
+@ticket_api.route("/tickets", methods=['GET', 'POST'])
+def getTickets():
+    tickets = []
+    if(request.method == 'POST'):
+        query_filter = ''
+        if request.json['filter']:
+            query_filter = request.json['filter']  
+        tickets = ticket_module.getTickets(query_filter)
+    
+    return jsonify({'tickets': tickets})
